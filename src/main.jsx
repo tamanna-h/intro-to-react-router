b90f527e7248e3fc4b7f93b6a37966a5cc4cde80
Tamanna <tahminadox@gmail.com>
2025-10-14 13:41:03 +0600

    added new routes for about, blogs, app, app2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
@@ -10,10 +10,28 @@ const router = createBrowserRouter([
     path: "/",
     element: <div>Welcome to React Router</div>,
   },
+  {
+    path: "about",
+    element: <div>About React Router</div>,
+  },
+  {
+    path: "blogs",
+    element: <div>All my blogs here</div>, 
+  },
+  {
+    path: "app",
+    Component: App
+  },
+  {
+    path: "app2",
+    // element: <App />,
+    Component: App
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>
 );
