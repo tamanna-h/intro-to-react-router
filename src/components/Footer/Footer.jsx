c04c994ea456880b347b80070265c165e782c55b
Tamanna <tahminadox@gmail.com>
2025-10-15 11:35:51 +0600

    integrate router <Outlet /> to render nested child routes and Navlink for active link

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Thank you for visiting our website</small>
+                <a href='/'>terms</a>
+                <a href='/'>privacy</a>
+                <a href='/fb'>facebook</a>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
             <h3>This is header</h3>
+            <nav>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/mobile'>Mobile</NavLink>
+                <NavLink to='/laptops'>Laptops</NavLink>
+            </nav>
         </div>
     );
 };
