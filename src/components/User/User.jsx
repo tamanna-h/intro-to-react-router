b8328fb5575011c1d2e607591f38fbef072f9220
Tamanna <tahminadox@gmail.com>
2025-10-16 07:07:28 +0600

    another example of Navigate and uses of useLocation

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,14 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate, useLocation } from 'react-router';
 import UserDetails2 from '../UserDetails2/UserDetails2';
 
 const User = ({user}) => {
     const [showInfo, setShowInfo] =useState(false);
+    const [visitHome, setVisitHome] = useState(false);
+    const location = useLocation();
+    console.log(location);
+    
+
     const {name, email, phone, id} = user;
 
     const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json());
@@ -13,6 +18,10 @@ const User = ({user}) => {
         padding: '10px',
         margin: '10px',
         borderRadius: '20px'
+    }
+
+    if(visitHome){
+        return <Navigate to="/"></Navigate>;
     }
 
     return (
@@ -27,6 +36,7 @@ const User = ({user}) => {
                     <UserDetails2 userPromise={userPromise}></UserDetails2>
                 </Suspense>
             }
+            <button onClick={() => setVisitHome(true)}>Visit Home</button>
         </div>
     );
 };
